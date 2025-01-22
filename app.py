ec0b6d375eec63764c87899895b8a20d661d1cb6
Sujal Jain <63149580+Sujal942@users.noreply.github.com>
2025-01-23 02:29:29 +0530

    update in port

--- app.py
+++ app.py
@@ -228,5 +228,5 @@ def add_space():
 #     port = int(os.environ.get("PORT", 8000))
     # app.run(host="0.0.0.0", port=port, debug=True)
 
-port = int(os.environ.get("PORT", 5000))  
+port = int(os.environ.get("PORT", 8000))  
 app.run(host="0.0.0.0", port=port, debug=False)
